a5655fa643f3699a369486ada892f91c7e6f4619
elainecristinap <elainecristinap98@gmail.com>
2025-05-13 21:12:40 -0300

    prototipo01

---
+++ Prototipo01/js/api.js
@@ -0,0 +1,55 @@
+const api = axios.create({
+  baseURL: "https://rickandmortyapi.com/api",
+});
+
+// Busca total de uma funcionaldidade
+async function getTotalByFeature(feature) {
+
+  try {
+    const result = await api.get(`/${feature}`);
+
+    return result.data.info.count;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Listagem dos personagens de acordo com a página selecionada
+async function listCharactersByPage(page = 1) {
+
+  try {
+    const result = await api.get("/character", {
+      params: { page },
+    });
+
+    return {
+      nextPage: result.data.info.next,
+      prevPage: result.data.info.prev,
+      charactersList: result.data.results,
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Busca o nome de um episódio de acordo com a URL fornecida
+async function getEpisodeDataFromUrl(url) {
+  try {
+    const result = await api.get(url);
+
+    return result.data.name;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Busca dados de um personagem por ID
+async function getCharacterById(characterId) {
+  try {
+    const result = await api.get(`/character/${characterId}`);
+
+    return result.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
